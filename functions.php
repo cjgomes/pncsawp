6888da03935f19d96b42eaf8c03700df3f39abaa
cjgomes <cjgomes.it@gmail.com>
2014-05-06 17:00:13 -0400

    começando a arrumar o slideshow da homepage

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function pncsa_setup() {
 	update_option('thumbnail_size_w', 290);
     update_option('thumbnail_size_h', 290);
     add_image_size( 'mini-thumbnail', 50, 50, true );
-    add_image_size( 'slide', 640, 290, true );
+    add_image_size( 'slide', 550, 300, true );
     register_nav_menu('Navigation', __('Navigation'));
     register_nav_menu('Publications-menu', __('Publications-menu'));
     register_nav_menu('Fasciculos-menu', __('Fasciculos-menu'));
@@ -246,6 +246,29 @@ function options_page() {
     </div>
 <?php
 }
+
+//improve excerpt
+
+function improved_trim_excerpt($text) {
+        global $post;
+        if ( '' == $text ) {
+                $text = get_the_content('');
+                $text = apply_filters('the_content', $text);
+                $text = str_replace('\]\]\>', ']]&gt;', $text);
+                $text = preg_replace('@<script[^>]*?>.*?</script>@si', '', $text);
+                $text = strip_tags($text, '<p>');
+                $excerpt_length = 70;
+                $words = explode(' ', $text, $excerpt_length + 1);
+                if (count($words)> $excerpt_length) {
+                        array_pop($words);
+                        array_push($words, '[...]');
+                        $text = implode(' ', $words);
+                }
+        }
+        return $text;
+}
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'improved_trim_excerpt');
 
 // Update options
 
